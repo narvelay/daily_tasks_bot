ccb6fbb89bdd213dd10af0afb1870e001e89dee5
narvelay <narvelay1@gmail.com>
2025-11-20 09:14:53 +0500

    models.py

--- models.py
+++ models.py
@@ -1,6 +1,6 @@
-# models.py
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.orm import declarative_base
+from datetime import datetime
 
 Base = declarative_base()
 
@@ -11,12 +11,5 @@ class User(Base):
     username = Column(String)
     fullname = Column(String)
     balance = Column(Integer, default=0)
-
-class Invoice(Base):
-    __tablename__ = "invoices"
-
-    id = Column(Integer, primary_key=True)
-    invoice_id = Column(Integer)
-    user_id = Column(Integer)
-    pack_id = Column(String)
-    status = Column(String)
+    last_reward_time = Column(DateTime, nullable=True)
+    last_task_time = Column(DateTime, nullable=True)
